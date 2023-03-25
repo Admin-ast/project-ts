d71be863d39bb6bbde653048f1f66b2a78bd0be2
Satyam Dahlan <dahlansatyam@gmail.com>
2023-03-25 15:53:59 +0530

    fix: npm issue fixed

--- src/components/panchang/GetPanchang.tsx
+++ src/components/panchang/GetPanchang.tsx
@@ -1,5 +1,7 @@
+import { format } from "date-fns";
+import Image from "next/image";
 import React from "react";
-import { Input } from "../forms";
+import Section from "../Section";
 
 type Props = {};
 
@@ -12,6 +14,7 @@ function GetPanchang({}: Props) {
             <input
               type="text"
               id="location"
+              defaultValue={"New Delhi, India"}
               className={`bg-[#FFF9EA]] w-full appearance-none rounded-xl border border-gray-400 px-12 !py-3 !text-[22px] text-sm text-gray-900 placeholder:text-[22px] placeholder:font-normal focus:border-[#0B8188] focus:outline-none`}
               placeholder={"Enter your city"}
               name={"location"}
@@ -27,6 +30,43 @@ function GetPanchang({}: Props) {
           </div>
         </div>
       </div>
+      <Section>
+        <div className="py-8 text-center lg:py-[55px] ">
+          <div className="space-y-4 pb-12">
+            <p className="font-[georgia] text-2xl font-semibold md:text-4xl">
+              Today’s Panchang
+            </p>
+            <p className="text-lg lg:text-[22px]">
+              {format(new Date(), "MMMM dd, EEEE")} | New Delhi, Delhi | India
+            </p>
+          </div>
+          <div className="grid grid-cols-12 gap-6">
+            <div className="col-span-8">
+              <div className="grid grid-cols-2 gap-8">
+                <div className="rounded-2xl bg-[#E2CB85] p-3">
+                  <p className="text-[22px]">Sunrise</p>
+                  <p className="text-xs">06:44 - 18:21</p>
+                </div>
+                <div className="rounded-2xl bg-[#E2CB85] p-3">
+                  <p className="text-[22px]">Moonrise</p>
+                  <p className="text-xs">14:34 - 04:18</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-span-4 flex flex-col justify-evenly rounded-2xl bg-[#FFF7E5] p-12">
+              <Image
+                src={"/assets/appointment/live-astro.png"}
+                alt={"astrologer"}
+                width={300}
+                height={370}
+                loading={"lazy"}
+                className="h-5/6 w-full object-contain"
+              />
+              <p className="text-base">Talk to our best Astrologers</p>
+            </div>
+          </div>
+        </div>
+      </Section>
     </div>
   );
 }
