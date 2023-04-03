05e99a5dccb7fb8f0d073413d10e2517a6ca4952
Satyam Dahlan <dahlansatyam@gmail.com>
2023-04-04 00:53:23 +0530

    feat: test api added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,17 @@ import Review from "@/components/home/Review";
 import Services from "@/components/home/Services";
 import Shop from "@/components/home/Shop";
 import WhoWeAre from "@/components/home/WhoWeAre";
+import { getFetcher } from "@/service";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const apiCall = async () => {
+      const result = await getFetcher("/");
+      console.log("result is", result);
+    };
+    apiCall();
+  }, []);
   return (
     <>
       <Hero />
