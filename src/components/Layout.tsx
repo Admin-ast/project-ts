3bfbd136a38458dca21e8943bf5e54a6319e4160
Satyam Dahlan <dahlansatyam@gmail.com>
2023-05-11 11:18:49 +0530

    feat: whatsapp support added

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import React from "react";
 import Footer from "./global/Footer";
 import Navbar from "./global/Navbar";
+import Link from "next/link";
+import Image from "next/image";
 
 type Props = {
   children: React.ReactNode;
@@ -16,10 +18,31 @@ function Layout({ children }: Props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/assets/main-logo.svg" />
       </Head>
-      <div className="flex min-h-screen flex-col font-[roboto]">
+      <div className="relative flex min-h-screen flex-col font-[roboto]">
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
+        <div
+          className="fixed right-8 bottom-8 md:bottom-12 md:right-20"
+          title="Chat with us"
+        >
+          <Link
+            href="https://wa.me/9413717889"
+            className="whatsapp_float"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="w-12 rounded-full shadow-2xl shadow-gray-900 md:w-16">
+              <Image
+                src="/assets/whatsapp-icon.png"
+                alt="whatsapp"
+                width={3100}
+                height={3100}
+                loading={"lazy"}
+              />
+            </div>
+          </Link>
+        </div>
       </div>
     </>
   );
