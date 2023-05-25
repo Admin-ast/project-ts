85de76803a090aac9a241e113fcfda899280f7f4
Ankit Negi <nankit793@gmail.com>
2023-05-25 12:06:41 +0500

    feat: blog details page

--- src/components/blog/Blog.tsx
+++ src/components/blog/Blog.tsx
@@ -1,108 +1,9 @@
 import React from "react";
 import Section from "../Section";
-import { BiCategory } from "react-icons/bi";
-import { AiFillFire } from "react-icons/ai";
-import { GrLanguage } from "react-icons/gr";
 import { FaSearch } from "react-icons/fa";
 import Link from "next/link";
 import { card } from "../../../utils/list";
-
-const categories = [
-  {
-    name: "Home",
-    url: "/",
-  },
-  {
-    name: "Tarot",
-    url: "/",
-  },
-  {
-    name: "Vastu",
-    url: "/",
-  },
-  {
-    name: "Vedic",
-    url: "/",
-  },
-  {
-    name: "Kundli",
-    url: "/",
-  },
-  {
-    name: "Sports",
-    url: "/",
-  },
-  {
-    name: "Transits",
-    url: "/",
-  },
-  {
-    name: "Festivals",
-    url: "/",
-  },
-  {
-    name: "Business",
-    url: "/",
-  },
-  {
-    name: "Gemstones",
-    url: "/",
-  },
-  {
-    name: "Numerology",
-    url: "/",
-  },
-  {
-    name: "Zodiac Signs",
-    url: "/",
-  },
-  {
-    name: "Compatibility",
-    url: "/",
-  },
-  {
-    name: "Entertainment",
-    url: "/",
-  },
-  {
-    name: "Current Affairs",
-    url: "/",
-  },
-  {
-    name: "Daily Horoscope",
-    url: "/",
-  },
-  {
-    name: "Mythological Tales",
-    url: "/",
-  },
-];
-const trending = [
-  {
-    title: "April Monthly Horoscope 2023",
-    url: "/",
-  },
-  {
-    title: "Why so many breakups in My Horoscope? Know the reason Why?",
-    url: "/",
-  },
-  {
-    title: "Why so many breakups in My Horoscope? Know the reason Why?",
-    url: "/",
-  },
-  {
-    title: "Why so many breakups in My Horoscope? Know the reason Why?",
-    url: "/",
-  },
-  {
-    title: "Why so many breakups in My Horoscope? Know the reason Why?",
-    url: "/",
-  },
-  {
-    title: "Why so many breakups in My Horoscope? Know the reason Why?",
-    url: "/",
-  },
-];
+import SideNavBlog from "./SideNavBlog";
 
 type Props = {};
 
@@ -111,52 +12,7 @@ const Blog = (props: Props) => {
     <div className="bg-[url('/assets/horoscope-bg.webp')] bg-cover bg-repeat py-8 lg:py-[51px]">
       <Section>
         <div className="flex items-start justify-between gap-6">
-          <div className="flex w-[350px] flex-col gap-2 border-r border-[#D9D9D9]">
-            <div className="flex items-center gap-1">
-              <BiCategory size={45} className="" />
-              <div>
-                <p className="text-2xl font-bold">Categories</p>
-                <p className="text-[10px]">Select Topic</p>
-              </div>
-            </div>
-            <hr className=" mr-5 w-[216px] border border-[#D9D9D9]" />
-            <div className="flex flex-col gap-2">
-              {categories.map((item, index) => (
-                <Link key={index} href={item?.url}>
-                  <p className="text-lg font-medium">{item?.name}</p>
-                </Link>
-              ))}
-            </div>
-            <div className="flex items-center gap-1 pt-5">
-              <AiFillFire size={45} className="" />
-              <div>
-                <p className="text-2xl font-bold">Trending Now</p>
-                <p className="text-[10px]">Trending Stories</p>
-              </div>
-            </div>
-            <hr className=" mr-5 w-[216px] border border-[#D9D9D9]" />
-            <div className="mr-6 flex flex-col gap-2">
-              {trending.map((item, index) => (
-                <Link key={index} href={item?.url}>
-                  <p className="text-justify font-medium">{item?.title}</p>
-                </Link>
-              ))}
-            </div>
-
-            <div className="flex items-center gap-1 pt-5">
-              <GrLanguage size={45} className="" />
-              <div>
-                <p className="text-2xl font-bold">Languages</p>
-                <p className="text-[10px]">Select Language</p>
-              </div>
-            </div>
-            <hr className=" mr-5 w-[216px] border border-[#D9D9D9]" />
-            <div className="flex flex-col gap-2">
-              <p>ALL LANGUAGES</p>
-              <p>HINDI</p>
-              <p>ENGLISH</p>
-            </div>
-          </div>
+          <SideNavBlog />
 
           <div className="flex w-full flex-col gap-2">
             <div className="flex items-center gap-2">
--- src/pages/blog/[blogdesc].tsx
+++ src/pages/blog/[blogdesc].tsx
@@ -1,8 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from "react";
 import { card } from "../../../utils/list";
 import { useRouter } from "next/router";
 import Section from "@/components/Section";
+import SideNavBlog from "@/components/blog/SideNavBlog";
+import { FaSearch } from "react-icons/fa";
+import Link from "next/link";
 import Blog from "@/components/blog/Blog";
+import Hero from "@/components/appoinments/Hero";
+import { HomeIcon } from "@heroicons/react/24/solid";
 
 type Props = {};
 
@@ -14,24 +20,42 @@ const BlogDesc = (props: Props) => {
   useEffect(() => {
     const result = card.filter((item) => item?.id == blogdesc);
     setBlogde(result);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <div>
-      <Section>
-        <div className="my-5 px-3 md:my-10 md:px-10">
-          <div className="my-3 text-[32px] font-bold">{blogde[0]?.text}</div>
-          <img
-            className=" auto mx-auto  rounded-[20px] object-cover object-top md:h-[342px] md:w-[514px]"
-            src={blogde[0]?.img}
-            alt=""
-          />
-          <div className="my-2 text-justify">{blogde[0]?.para}</div>
-          <div className="my-6">
-            <Blog />
+    <>
+      <div className="bg-[url('/assets/horoscope-bg.webp')] bg-cover bg-repeat ">
+        <Hero text="Blog" icon="" />
+        <Section>
+          <div className="flex items-center space-x-2">
+            <HomeIcon className="h-6 w-6 bg-[#D9D9D9] p-1" />
+            <p className="bg-[#C6A65A] p-1 px-4 text-[10px] font-medium">
+              Blog
+            </p>
           </div>
-        </div>
-      </Section>
-    </div>
+        </Section>
+        <Section>
+          <div className="flex items-start justify-between gap-6 pt-8">
+            <SideNavBlog />
+            <div>
+              <Section>
+                <div className="px-3 md:px-10">
+                  <div className=" text-[32px] font-bold">
+                    {blogde[0]?.text}
+                  </div>
+                  <img
+                    className=" auto mx-auto  rounded-[20px] object-cover object-top md:h-[342px] md:w-[514px]"
+                    src={blogde[0]?.img}
+                    alt=""
+                  />
+                  <div className="my-2 text-justify">{blogde[0]?.para}</div>
+                </div>
+              </Section>
+            </div>
+          </div>
+        </Section>
+      </div>
+    </>
   );
 };
 
