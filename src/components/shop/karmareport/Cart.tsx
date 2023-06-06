4c8a128ca2aecfee442be357222e68ca2225faa0
devarshi009 <devarshiojha009@gmail.com>
2023-06-06 20:31:21 +0530

    feat: karmaReport and loveScore page added

---
+++ src/components/shop/karmareport/Cart.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { BsFillCartFill } from "react-icons/bs";
+
+type Props = {};
+
+const Cart = (props: Props) => {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="flex justify-end px-10">
+      <BsFillCartFill className="text-[18px]" />:{count}
+    </div>
+  );
+};
+
+export default Cart;
