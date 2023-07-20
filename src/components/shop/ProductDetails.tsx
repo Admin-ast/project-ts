32d0a8e88376158fc47ac138567ba5b4f34aceb7
devarshi009 <devarshiojha009@gmail.com>
2023-07-20 14:47:11 +0530

    feat: productDetails page

--- src/components/shop/ProductDetails.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const ProductDetails = (props: Props) => {
-  return <div>ProductDetails</div>;
-};
-
-export default ProductDetails;
--- src/components/shop/minicart/CartDrawer.tsx
+++ src/components/shop/minicart/CartDrawer.tsx
@@ -19,7 +19,7 @@ const CartDrawer = ({ isOpen, onClose }: Props) => {
   return (
     <div
       className={`fixed inset-y-0 right-0 z-50 w-full transform bg-white shadow-lg md:w-1/2 lg:w-1/4 
-      ${drawerClasses}  transition-transform duration-300 ease-in-out`}
+      ${drawerClasses}  transition-transform duration-300 ease-in-out `}
     >
       <div className="flex justify-between px-4 py-2">
         <h2 className="mt-2 text-lg font-semibold text-gray-600">
@@ -36,26 +36,54 @@ const CartDrawer = ({ isOpen, onClose }: Props) => {
       </div>
 
       {/* Add your cart items and other content here */}
+      <div className="grid grid-rows-3 ">
+        <div className="flex gap-2 p-5 hover:bg-slate-100">
+          <div className="">
+            <img
+              src="https://media.istockphoto.com/id/1048293726/photo/large-five-face-rudraksh-seed-macro-in-hand.webp?b=1&s=170667a&w=0&k=20&c=VyA3fwBLGANVlHjpyG5QQ4qmF655LA6ZN6TW-aLO1iw="
+              alt=""
+              className=""
+            />
+          </div>
+          <div className="">
+            <p className="text-base font-semibold text-black ">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi
+              recusandae accusamus, nisi
+            </p>
+            <div className="flex w-[55%] rounded-[15px] border-[1px]">
+              <button
+                disabled={quantity === 1}
+                onClick={handleDec}
+                className="  border-1 border-r px-3 py-2 text-black hover:bg-teal-900"
+              >
+                -
+              </button>
+              <p className="border-1 border-r px-3 py-2 text-black">
+                {quantity}
+              </p>
+              <button
+                onClick={handleInc}
+                className="px-3 py-2 text-black hover:bg-teal-900"
+              >
+                +
+              </button>
+            </div>
+            <p className="text-black">₹393</p>
+          </div>
+        </div>
 
-      <div className="p-10">
-        <img
-          src="https://media.istockphoto.com/id/1048293726/photo/large-five-face-rudraksh-seed-macro-in-hand.webp?b=1&s=170667a&w=0&k=20&c=VyA3fwBLGANVlHjpyG5QQ4qmF655LA6ZN6TW-aLO1iw="
-          alt=""
-          className=""
-        />
-        <p className="text-black">Price:800 rs</p>
-        <div className="flex ">
-          <button
-            disabled={quantity === 1}
-            onClick={handleDec}
-            className="bg-teal-400 px-5 text-black"
-          >
-            -
-          </button>
-          <p className="px-5 text-black">{quantity}</p>
-          <button onClick={handleInc} className="bg-teal-400 px-5 text-black">
-            +
-          </button>
+        {/* <div className=""></div> */}
+        <div className="border-top border-[2px] p-5 text-black ">
+          <div className="flex justify-between text-xl  ">
+            <p className="">Subtotal:</p>
+            <p className="">₹393</p>
+          </div>
+          <div className="text-bold mt-5 bg-teal-500 py-3 text-center text-white">
+            <button className="">VIEW CART</button>
+          </div>
+          <div className="text-bold mt-5 bg-teal-500 py-3 text-center text-white">
+            <button className="">CHECKOUT</button>
+          </div>
         </div>
       </div>
     </div>
