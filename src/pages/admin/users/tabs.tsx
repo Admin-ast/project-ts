d8afb6e2f765a5321288aca98df2c5c769d6fb8c
devarshi009 <devarshiojha009@gmail.com>
2023-11-02 17:33:47 +0530

    feat: order history

---
+++ src/pages/admin/users/tabs.tsx
@@ -0,0 +1,12 @@
+import Tabsss from "@/components/Main Admin/Users/Tabs";
+import React from "react";
+
+const TabsPage2 = () => {
+  return (
+    <div>
+      <Tabsss />
+    </div>
+  );
+};
+
+export default TabsPage2;
